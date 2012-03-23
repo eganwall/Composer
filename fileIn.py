d108cb61c4b20875ff48cf6b81e8791b8aeab321
Egan Wall <eganwall@yahoo.com>
2012-03-22 21:43:31 -0400

    Also changed fileIn.py to batch-read/write the entire population in a single execution.

--- fileIn.py
+++ fileIn.py
@@ -1,43 +1,60 @@
 from midiutil.MidiFile import MIDIFile
 
-input = open('music3.dna', 'r').read()
-genes = input.split('|')
-genes.remove('')
-print input
-print genes
+m = 0 # our population counter
 
-thisAlgorithmBecomingSkynetCost = 999999999 # this will ALWAYS be included in the fitness function
+while(m < 20):
+	
+	# increment our population counter
+	m = m + 1
+	
+	# change the files we're pointing to
+	fileName = "music" + str(m) + ".dna"
+	outName = "Outputs/output" + str(m) + ".mid"
+	
+	print("Now reading file %s" % fileName)
+	
+	# now we'll read in the genetic material
+	input = open(fileName, 'r').read()
+	
+	# clean it up a little bit so we get just the straight numbers yo
+	genes = input.split('|') 
+	genes.remove('')
+	
+	print input
+	print genes
 
-myMIDI = MIDIFile(1)
+	thisAlgorithmBecomingSkynetCost = 999999999 # this will ALWAYS be included in the fitness function
 
-# now we'll initialize the attributes that will stay constant 
-# among all of the notes
-track = 0
-channel = 0
-time = 0
-volume = 100
+	myMIDI = MIDIFile(1)
 
-myMIDI.addTrackName(track, time, "Genetic material test")
-myMIDI.addTempo(track, time, 120)
+	# now we'll initialize the attributes that will stay constant 
+	# among all of the notes
+	track = 0
+	channel = 0
+	time = 0
+	volume = 100
+	
+	myMIDI.addTrackName(track, time, "Genetic material test")
+	myMIDI.addTempo(track, time, 120)
 
-# now we can loop through the data that we read from the
-# input file and then we'll write that shit to our MIDI
-for gene in genes:
-	if not gene:
-		break
-	pitch = int(gene[0:2])
-	duration = float(gene[2:4]) / 10
+	# now we can loop through the data that we read from the
+	# input file and then we'll write that shit to our MIDI
+	for gene in genes:
+		if not gene:
+			break
+		pitch = int(gene[0:2])
+		duration = float(gene[2:4]) / 10
 
-	print("Adding pitch: %d, duration: %.1f to the file" % (pitch, duration))
-	myMIDI.addNote(track, channel, pitch, time, duration, volume)
+		print("Adding pitch: %d, duration: %.1f from the file %s" % (pitch, duration, fileName))
+		myMIDI.addNote(track, channel, pitch, time, duration, volume)
 
-	time += duration
+		time += duration
 
-print("The piece is %.1f beats long" % time) # make sure the length is correct
+	print("The piece is %.1f beats long" % time) # make sure the length is correct
 
-# and then we write it to our output MIDI file	
-outFile = open("output.mid", 'wb')
-myMIDI.writeFile(outFile)
+	# and then we write it to our output MIDI file	
+	outFile = open(outName, 'wb')
+	myMIDI.writeFile(outFile)
 
-# bitches ain't shit
-outFile.close()
+	# bitches ain't shit
+	outFile.close()
