a51dcc9c5621665469ffe637e99ceeefddcc035d
Egan Wall <eganwall@yahoo.com>
2012-03-26 13:08:19 -0400

    Fitness function will now select four finalists for tournament selection, and then determine the ultimate winners of the game of life.

--- fitness.py
+++ fitness.py
@@ -5,6 +5,7 @@
 	"Twinkle, Twinkle Little Star." Yep, 
 	it's just as badass as it sounds.
 '''
+import random
 
 # our population counter
 m = 0
@@ -58,3 +59,40 @@ while(m < 20):
 	if(totalPitches != TARGET_TOTAL):
 		fitnesses[m - 1] += abs(totalPitches - TARGET_TOTAL)
 	print("The pitch total of piece %d is %d, fitness is %d." % (m, totalPitches, fitnesses[m - 1]))
+	
+	''' INSERT MORE FITNESS CALCULATIONS HERE WHEN YOU THINK OF THEM '''
+	
+# now that we've analyzed the fitnesses of our population, we can
+# choose two pairings for our tournament selection
+finalistPieces = list()
+finalistFits = list()
+i = 0
+
+while(i < 4):
+	i += 1
+	finalist = random.randrange(0, len(fitnesses))
+	
+	# we'll add the fitness and number of our finalist piece to the arrays
+	finalistFits.append(fitnesses[finalist])
+	finalistPieces.append(finalist)
+	
+print("The four finalists are:")
+print finalistPieces
+print finalistFits
+
+# select the two winners
+if(finalistFits[0] <= finalistFits[1]):
+	finalist1Fit = finalistFits[0]
+	finalist1 = finalistPieces[0]
+else:
+	finalist1Fit = finalistFits[1]
+	finalist1 = finalistPieces[1]
+if(finalistFits[2] <= finalistFits[3]):
+	finalist2Fit = finalistFits[2]
+	finalist2 = finalistPieces[2]
+else:
+	finalist2Fit = finalistFits[3]
+	finalist2 = finalistPieces[3]
+
+print("Parent 1 is piece number %d, with fitness %d." % (finalist1, finalist1Fit))
+print("Parent 2 is piece number %d, with fitness %d." % (finalist2, finalist2Fit))
